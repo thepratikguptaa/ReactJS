ea58880d0b5123330844117d4a9699d84386b286
thepratikguptaa <pratikguptamain@gmail.com>
2025-04-14 22:21:30 +0530

    reactjs

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,13 @@
+import Chai from './chai'
+function App() {
+
+  return (
+    <>
+    <chai/>
+    <h3>abhi ek se jyada element return kar sakte hai</h3>
+    <p>fragment use karke</p>
+    </>
+  )
+}
+export default App
+
---
+++ 01vitereact/src/chai.jsx
@@ -0,0 +1,6 @@
+function Chai() {
+    return (
+        <h2>Chai taiyaar hai</h2>
+    )
+}
+export default Chai
