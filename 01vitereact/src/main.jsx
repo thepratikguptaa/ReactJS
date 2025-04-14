e01c4a03d45d57a1002114d0fb5fbe556c5bd873
thepratikguptaa <pratikguptamain@gmail.com>
2025-04-15 00:45:56 +0530

    reactjs

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,8 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const anotherElement = {
+  <a href="http://www.google.com" target="_blank">Click me to visit Google</a>
+}
+ReactDOMcreateRoot(document.getElementById('root')).render(
+  
+  // <App />
+  anotherElement
 )
