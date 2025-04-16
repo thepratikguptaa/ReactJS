9f17130ae978d58eb255436f0944c43b123103fc
thepratikguptaa <pratikguptamain@gmail.com>
2025-04-16 13:48:03 +0530

    tailwind-props

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -5,9 +5,23 @@ import Card from './components/Card'
 function App() {
   return (
     <>
-      <h1 className=' text-blue-400 p-4 rounded-xl mb-4'>TailwindCSS</h1>
-      <Card username="TailwindCSS" imgSrc="https://images.pexels.com/photos/29445974/pexels-photo-29445974/free-photo-of-close-up-of-colorful-javascript-code-on-screen.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" intro="Rapidly build modern websites without ever leaving your HTML." btnText="Learn more" btnLink="https://tailwindcss.com/"/>
-      <Card username="Pratik Gupta" imgSrc="https://images.pexels.com/photos/28991288/pexels-photo-28991288.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" intro="Hey there!" btnText="Visit Profile" btnLink="https://github.com/thepratikguptaa"/>
+      <h1 className='text-blue-400 p-4 rounded-xl mb-4'>TailwindCSS</h1>
+      <div className="flex flex-col md:flex-row gap-4">
+        <Card 
+          username="TailwindCSS" 
+          imgSrc="https://images.pexels.com/photos/29445974/pexels-photo-29445974/free-photo-of-close-up-of-colorful-javascript-code-on-screen.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" 
+          intro="Rapidly build modern websites without ever leaving your HTML." 
+          btnText="Learn more" 
+          btnLink="https://tailwindcss.com/" 
+        />
+        <Card 
+          username="Pratik Gupta" 
+          imgSrc="https://images.pexels.com/photos/28991288/pexels-photo-28991288.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" 
+          intro="Hey there!" 
+          btnText="Visit Profile" 
+          btnLink="https://github.com/thepratikguptaa" 
+        />
+      </div>
     </>
   )
 }
