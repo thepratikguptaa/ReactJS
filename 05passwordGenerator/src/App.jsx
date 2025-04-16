c229739d10c710c551b9e9ba57151fa75c0e24ad
thepratikguptaa <pratikguptamain@gmail.com>
2025-04-17 00:46:14 +0530

    password generator

---
+++ 05passwordGenerator/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [length, setLength] = useState(8)
+
+  return (
+    <>
+      <h1 className='text-4xl text-center text-white'>Password Generator</h1>
+    </>
+  )
+}
+
+export default App
