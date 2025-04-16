22caf0ffe7fbbc834c5e35f03ed8e11a56b99c60
thepratikguptaa <pratikguptamain@gmail.com>
2025-04-17 02:57:39 +0530

    password generator

--- 05passwordGenerator/src/App.jsx
+++ 05passwordGenerator/src/App.jsx
@@ -3,7 +3,11 @@ import './App.css'
 
 function App() {
   const [length, setLength] = useState(8)
+  const [numberAllowed, setNumberAllowed] = useState(false)
+  const [charAllowed, setCharAllowed] = useState(false)
+  const [password, setPassword] = useState("")
 
+  const passwordGenerator = () =>
   return (
     <>
       <h1 className='text-4xl text-center text-white'>Password Generator</h1>
