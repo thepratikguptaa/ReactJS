a7e14a4b1a13fda771753e471ca375b6530e03d0
thepratikguptaa <pratikguptamain@gmail.com>
2025-04-16 13:13:44 +0530

    tailwind-props

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -1,16 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Card from './components/Card'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <h1 className=' text-blue-400 p-4 rounded-xl mb-4'>TailwindCSS</h1>
-      <Card/>
+      <Card username="TailwindCSS" imgSrc="https://images.pexels.com/photos/29445974/pexels-photo-29445974/free-photo-of-close-up-of-colorful-javascript-code-on-screen.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" intro="Rapidly build modern websites without ever leaving your HTML." btnText="Learn more" btnLink="https://tailwindcss.com/"/>
+      <Card username="Pratik Gupta" imgSrc="https://images.pexels.com/photos/28991288/pexels-photo-28991288.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" intro="Hey there!" btnText="Visit Profile" btnLink="https://github.com/thepratikguptaa"/>
     </>
   )
 }
--- 03tailwindprops/src/components/Card.jsx
+++ 03tailwindprops/src/components/Card.jsx
@@ -1,26 +1,28 @@
 import React from 'react'
 
-function card() {
+function card({username, imgSrc, intro, btnText, btnLink}) {
+    console.log(username);
+    
   return (
     <div className="max-w-xs rounded-md shadow-md bg-black text-gray-100">
         <img
-          src="https://images.pexels.com/photos/29445974/pexels-photo-29445974/free-photo-of-close-up-of-colorful-javascript-code-on-screen.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+          src={imgSrc}
           alt=""
           className="object-cover object-center w-full rounded-t-md h-72 bg-gray-500"
         />
         <div className="flex flex-col justify-between p-6 space-y-8">
           <div className="space-y-2">
-            <h2 className="text-3xl font-semibold tracking-wide">TailwindCSS</h2>
+            <h2 className="text-3xl font-semibold tracking-wide">{username}</h2>
             <p className="text-gray-400">
-            Rapidly build modern websites without ever leaving your HTML.
+            {intro}
             </p>
           </div>
           <button
             type="button"
             className="flex items-center justify-center w-full p-3 font-semibold tracking-wide rounded-md bg-gray-800 text-gray-200"
-            onClick={() => window.open('https://tailwindcss.com/', '_blank')}
+            onClick={() => window.open(btnLink, '_blank')}
           >
-            Read more
+            {btnText}
           </button>
         </div>
       </div>
