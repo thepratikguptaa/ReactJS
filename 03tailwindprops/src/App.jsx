a11656155692159ff87fa7919bfe699bdec6dc2b
thepratikguptaa <pratikguptamain@gmail.com>
2025-04-16 12:04:01 +0530

    tailwind-basics

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -8,26 +8,19 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1 className='bg-green-400 text-black p-4 rounded-xl'>Testing TailwindCSS</h1>
+      <div class="flex flex-col items-center gap-6 p-7 md:flex-row md:gap-8 rounded-2xl">
+  <div>
+    <img class="size-48 shadow-xl rounded-md" alt="" src="https://images.pexels.com/photos/29445974/pexels-photo-29445974/free-photo-of-close-up-of-colorful-javascript-code-on-screen.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" />
+  </div>
+  <div class="flex items-center md:items-start">
+    <span class="text-2xl font-medium">Learning TailwindCSS Basics</span>
+    <span class="font-medium text-sky-500">It makes everything easier</span>
+    <span class="flex gap-2 font-medium text-gray-600 dark:text-gray-400">
+      <span>Loving it</span>
+    </span>
+  </div>
+</div>
     </>
   )
 }
