c1ccc60239747f931f1561083c542611e931bb50
thepratikguptaa <pratikguptamain@gmail.com>
2025-04-19 00:33:51 +0530

    currency convertor

---
+++ 06currencyConvertor/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-3xl font-bold '>Currency Convertor</h1>
+    </>
+  )
+}
+
+export default App
