17c4961fd9ee3fd20595fbecbf75dc0737346a7e
thepratikguptaa <pratikguptamain@gmail.com>
2025-05-22 13:16:02 +0530

    react router

--- 07reactRouter/src/components/Github/Github.jsx
+++ 07reactRouter/src/components/Github/Github.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Github() {
   return (
     <div className='text-3xl text-center p-4'><p className='font-bold m-4 text-black'> Github Contributions</p>
-    <img className='mx-auto' src="http://ghchart.rshah.org/thepratikguptaa" alt="2016rshah's Github chart" /></div>
+    <img className='mx-auto' src="http://ghchart.rshah.org/thepratikguptaa" alt="thepratikguptaa's Github chart" /></div>
   )
 }
 
