7a485339da512d7996d3c759a37880d9eec86b7a
thepratikguptaa <pratikguptamain@gmail.com>
2025-05-23 23:43:29 +0530

    theme switch using context API

--- 09themeSwitcher/src/App.jsx
+++ 09themeSwitcher/src/App.jsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from 'react'
 import './App.css'
 import { ThemeProvider } from './contexts/theme'
 import ThemeBtn from './components/ThemeButton'
-import Card from './components/card'
+import Card from './components/Card'
 
 function App() {
   const [themeMode, setThemeMode] = useState("light")
